041cbad5c5b2935eee83969ac2bba430cfb2cc56
number09 <cielo.abierto09@gmail.com>
2018-03-21 07:31:38 +0900

    tutorial 9

---
+++ tutorial9/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Navigation />
+    <Friends />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation'
+import Friends from './components/Friends'
+import FooterComponent from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Navigation,
+    Friends,
+    FooterComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tutorial9/src/components/Friends.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>My Friends</h1>
+    <li v-for="friend in FriendsStore.friends">{{friend}}</li>
+    <input v-model="newFriend" v-on:keyup.13="addFriend(newFriend)"/>
+  </div>
+
+</template>
+
+<script>
+import FriendsStore from "../stores/FriendStore"
+  export default {
+    data() {
+     return{
+       newFriend:null,
+       FriendsStore:FriendsStore.data,
+
+     };
+    },
+    methods :{
+      addFriend(name) {
+        FriendsStore.methods.addFriend(name)
+        this.newFriend = null;
+
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
